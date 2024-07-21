1ab9f9dd132b2674d9c1135d2a564fa27b90055a
ALIATUL IZZAH BINTI JASMAN <izzahalia6@gmail.com>
2024-07-21 21:34:34 +0800

    To Do App

---
+++ ToDoApp/to_do_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+        ),
+      ),
+    );
+  }
+}
---
+++ ToDoApp/to_do_app/lib/pages/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  //list of todo task
+
+  List toDoList = [
+    ["Cooking",false,],
+    ["Swimming",false,],
+
+  ];
+
+  void checkBoxChanged(bool? value, int index){
+
+    setState((){
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void createNewTask(){
+    showDialog(context: context, builder: (context){
+      return AlertDialog();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 184, 140, 203),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Align(
+          alignment: Alignment.center,
+          child: const Text('To Do'),
+        ),
+      ),
+
+    floatingActionButton: FloatingActionButton(
+      onPressed: createNewTask,
+      child: Icon(Icons.add),
+    ),
+
+
+      body: ListView.builder(
+
+        itemCount: toDoList.length,
+        itemBuilder: (context,index){
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value,index), );
+        }
+
+      )
+      
+    );
+  }
+}
